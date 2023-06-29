eb22bb0b90c7e6475eb82387414c73d2c80f6b19
Cesar Ochoa <cesar_8ab@hotmail.com>
2023-06-28 21:55:55 -0600

    Retake the course

--- js/math.js
+++ js/math.js
@@ -1,1 +1,80 @@
-alert('math')
+console.group('Cuadrado');
+
+const ladoCuadrado = 5
+const perimetroCuadrado = ladoCuadrado * 4
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+
+console.log({
+    ladoCuadrado,
+    perimetroCuadrado,
+    areaCuadrado
+});
+
+function calcularCuadrado(lado){
+    return {
+        perimetro: lado * 4,
+        area: lado * lado,
+    }
+}
+
+console.groupEnd('Cuadrado')
+console.group('Triangulo')
+const ladoTriangulo1 = 6
+const ladoTriangulo2 = 6
+const ladoTrianguloBase = 4
+const alturaTriangulo = 5.5
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2
+
+function calcularTriangulo(lado1, lado2, base, altura){
+    return {
+        perimetro: lado1 + lado2 + base,
+        area: (base * altura)/2,
+    }
+}
+
+function calcularAlturaTriangulo(lados, base){
+    if(lados === base){
+        console.warn('No es un isosceles')
+    } else{
+        return Math.sqrt( (lados**2) - ( (base**2) / 4 ) )
+    }
+}
+
+console.log({
+    ladoTriangulo1,
+    ladoTriangulo2,
+    ladoTrianguloBase,
+    alturaTriangulo,
+    perimetroTriangulo,
+    areaTriangulo
+});
+console.groupEnd('Triangulo')
+
+console.group('Circle')
+const radioCirculo = 3
+const diametroCirculo = radioCirculo * 2
+const pi = 3.1415
+
+const circunferencia = diametroCirculo * pi
+const areaCirculo = (radioCirculo ** 2) * pi
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    pi,
+    circunferencia,
+    areaCirculo,
+});
+
+function calcularCirculo(radio){
+    const diametro = radio * 2
+    const radioAlCuadrado = Math.pow(radio,2)
+    return {
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI
+    }
+}
+
+console.groupEnd('Circle')
